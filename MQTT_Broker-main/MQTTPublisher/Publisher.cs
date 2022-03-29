ea32906106b9af316253f22b6056d2d6042dc075
LeonardoSonsin <70351101+LeonardoSonsin@users.noreply.github.com>
2022-03-29 19:49:39 -0300

    Add files via upload

---
+++ MQTT_Broker-main/MQTTPublisher/Publisher.cs
@@ -0,0 +1,59 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace MQTTPublisher
+{
+    class Publisher
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                        .WithClientId(Guid.NewGuid().ToString())
+                        .WithTcpServer("test.mosquitto.org", 1883) 
+                        .WithCleanSession()
+                        .Build();
+            client.UseConnectedHandler(e =>    
+            {
+                Console.WriteLine("Conectado ao broker com sucesso ");
+            });
+
+            client.UseDisconnectedHandler(e =>  
+            {
+                Console.WriteLine("Desconectado do broker com sucesso");
+            });
+
+            await client.ConnectAsync(options); 
+
+            Console.WriteLine("Por favor, pressione uma tecla para publicar a mensagem"); 
+            Console.ReadLine();
+
+           
+            await PublishMessageAsync(client);
+            Console.WriteLine("Para sair presione uma tecla");
+            Console.ReadLine();
+            await client.DisconnectAsync(); 
+        }
+
+        static async Task PublishMessageAsync(IMqttClient client) 
+            Console.WriteLine("Por favor, digite a mensagem");
+            var mensagem = Console.ReadLine();
+            string messagePayLoad = $"{mensagem} ";
+            var message = new MqttApplicationMessageBuilder()
+                        .WithTopic("BrokerTeste") 
+                        .WithPayload(messagePayLoad)
+                        .WithAtLeastOnceQoS()
+                        .Build();
+
+            if (client.IsConnected)  
+            {
+                await client.PublishAsync(message);
+                Console.WriteLine($"mensagem publicada - {messagePayLoad}");
+            }
+        }
+    }
+}
---
+++ MQTT_Broker-main/MQTTSubscriber/Subscriber.cs
@@ -0,0 +1,71 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQTTSubscriber
+{
+    class Subscriber
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                        .WithClientId(Guid.NewGuid().ToString())
+                        .WithTcpServer("test.mosquitto.org", 1883)
+                        .WithCleanSession()
+                        .Build();
+            client.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("Conectado ao broker");
+                var topicFilter = new TopicFilterBuilder()   
+                            .WithTopic("BrokerTeste")
+                            .Build();
+
+                await client.SubscribeAsync(topicFilter); 
+            });
+
+            client.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Desconectado do broker");
+            });
+
+            client.UseApplicationMessageReceivedHandler(async e =>  
+            {
+                var Recebido = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                Console.WriteLine($"mensagem recebida {Recebido}");
+                if(Recebido!=null) 
+                {
+                    var httpCliente = new HttpClient();
+                    var objeto = new { mensagem = Recebido };
+                    var content = ToRequest(objeto);
+                    var response = await httpCliente.PostAsync("https://localhost:8080/Mensagem", content);
+
+                }
+
+            });
+
+            
+            await client.ConnectAsync(options);
+
+            Console.ReadLine();
+
+            await client.DisconnectAsync();
+
+
+        }
+
+  
+        private static StringContent ToRequest(object obj) 
+        {
+            var json = JsonConvert.SerializeObject(obj);
+            var formatado = new StringContent(json, Encoding.UTF8, "application/json");
+            return formatado;
+        }
+    }
+}
